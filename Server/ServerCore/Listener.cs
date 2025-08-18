34f5ac62baeec01873c0e3881aa4917e6aed18ca
HongJiBeom <hongjibeom@hongjibeom-ui-MacBookPro.local>
2025-08-18 14:02:42 +0900

    feat: ServerCore lib 생성 & 소켓 리스너 생성

---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore;
+
+public class Listener
+{
+    private Socket _listenSocket;
+    
+    public void Init(IPEndPoint endPoint, int register = 10, int backlog = 100)
+    {
+        _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        
+        _listenSocket.Bind(endPoint);
+        
+        _listenSocket.Listen(backlog);
+
+        for (int i = 0; i < register; i++)
+        {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+    }
+
+    void RegisterAccept(SocketAsyncEventArgs args)
+    {
+        args.AcceptSocket = null;
+
+        bool pending = _listenSocket.AcceptAsync(args);
+        if (pending == false)
+            OnAcceptCompleted(null, args);
+    }
+
+    void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+    {
+        try
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        
+        RegisterAccept(args);
+    }
+}
