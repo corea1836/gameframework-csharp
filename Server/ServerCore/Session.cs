13d81eac3d0b01b724b0aff649141141d0d74da3
HongJiBeom <hongjibeom@hongjibeom-ui-MacBookPro.local>
2025-08-18 16:24:32 +0900

    feat: Session에 RecvBuffer 연결

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -5,10 +5,18 @@ namespace ServerCore;
 public class Session
 {
     private Socket _socket;
+    private int _disconnected = 0;
+
+    private RecvBuffer _recvBuffer = new RecvBuffer(65535);
 
     private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
     private SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
 
+    public int OnRecv(ArraySegment<byte> segment)
+    {
+        return 0;
+    }
+    
     public void Start(Socket socket)
     {
         _socket = socket;
@@ -21,6 +29,9 @@ public class Session
 
     public void Disconnect()
     {
+        if (Interlocked.Exchange(ref _disconnected, 1) == 1)
+            return;
+        
         _socket.Shutdown(SocketShutdown.Both);
         _socket.Close();
     }
@@ -34,6 +45,13 @@ public class Session
 
     void RegisterRecv()
     {
+        if (_disconnected == 1)
+            return;
+        
+        _recvBuffer.Clean();
+        ArraySegment<byte> segment = _recvBuffer.WriteSegment;
+        _recvArgs.SetBuffer(segment.Array, segment.Offset, segment.Count);
+        
         try
         {
             bool pending = _socket.ReceiveAsync(_recvArgs);
@@ -46,17 +64,32 @@ public class Session
         }
     }
 
-
-
     void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
     {
         if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
         {
             try
             {
-                Console.WriteLine("OnRecvCompleted Success");
-                //TODO: 컨텐츠 쪽으로 데이터를 넘겨준 후 처리 결과 확인
+                if (_recvBuffer.OnWrite(args.BytesTransferred) == false)
+                {
+                    Disconnect();
+                    return;
+                }
 
+
+                int processLen = OnRecv(_recvBuffer.ReadSegment);
+                if (processLen < 0 || _recvBuffer.DataSize < processLen)
+                {
+                    Disconnect();
+                    return;
+                }
+
+                if (_recvBuffer.OnRead(processLen) == false)
+                {
+                    Disconnect();
+                    return;
+                }
+                
                 RegisterRecv();
             }
             catch (Exception e)
